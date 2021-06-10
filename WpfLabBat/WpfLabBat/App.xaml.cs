40b6490956bf8fc99092509caf374a143d2ada8b
RuslanSmir <78635318+RuslanSmir@users.noreply.github.com>
2021-06-10 14:56:04 +0300

    Update App.xaml.cs

--- WpfLabBat/WpfLabBat/App.xaml.cs
+++ WpfLabBat/WpfLabBat/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WpfLabBat
+namespace WpfLabSmirnov
 {
     /// <summary>
     /// Логика взаимодействия для App.xaml
