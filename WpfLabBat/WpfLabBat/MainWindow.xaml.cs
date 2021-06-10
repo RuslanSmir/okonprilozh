6c817634ddfbd0ef78f4aca5cf5164804767a9df
RuslanSmir <78635318+RuslanSmir@users.noreply.github.com>
2021-06-10 14:52:41 +0300

    Update MainWindow.xaml.cs

--- WpfLabBat/WpfLabBat/MainWindow.xaml.cs
+++ WpfLabBat/WpfLabBat/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfLabBat
+namespace WpfLabSmirnov
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
